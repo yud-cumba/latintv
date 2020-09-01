e12fa17c319f8ff98bd7a134414864605f6aceda
Yudith <yudith@MacBook-Air-de-administrador.local>
2020-08-31 21:33:42 -0500

    boilerplate

--- latintv/src/Components/Header.jsx
+++ latintv/src/Components/Header.jsx
@@ -3,12 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <div>
-                <img className='logo' src ={logo} alt="Logo Burger"/>
-            </div>
-            <div>
-                <Link className='elegirUsuario' to={referencia}>Iniciar sesión</Link>
-            </div>
+            header
         </header>
     )
 };
