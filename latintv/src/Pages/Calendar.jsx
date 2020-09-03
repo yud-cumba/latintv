8e9e0668b06a7fec7ff61b6eb16199ff27b51557
IsabelaSanchez <natyst_26@hotmail.com>
2020-09-03 02:19:12 -0500

    new changes

--- latintv/src/Pages/Calendar.jsx
+++ latintv/src/Pages/Calendar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Header from '../Components/Header'
 import Week from '../Components/Week'
 import Month from '../Components/Month'
 
@@ -6,6 +7,7 @@ import Month from '../Components/Month'
 export default function Calendar(props) {
     return (
         <div>
+            <Header/>
             <Week/>
             <Month/>
         </div>
