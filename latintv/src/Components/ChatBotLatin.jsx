bebba72261133d4caa8fc4ba843cd2ae4516ab98
Yudith <yudith@MacBook-Air-de-administrador.local>
2020-09-03 15:08:47 -0500

    chat bot inicial

--- latintv/src/Components/ChatBotLatin.jsx
+++ latintv/src/Components/ChatBotLatin.jsx
@@ -8,24 +8,39 @@ export default function ChatBotLatin() {
     steps={[
       {
         id: '1',
-        message: 'What number I am thinking?',
+        message: 'Soy Latinobot ¿Con quién tengo el gusto de conversar?',
         trigger: '2',
       },
       {
         id: '2',
-        options: [
-          { value: 1, label: 'Number 1', trigger: '4' },
-          { value: 2, label: 'Number 2', trigger: '3' },
-          { value: 3, label: 'Number 3', trigger: '3' },
-        ],
+        user: true,
+        trigger: '3',
       },
       {
         id: '3',
-        message: 'Wrong answer, try again.',
-        trigger: '2',
+        message: 'Hola {previousValue}!, ¡Un gusto de conocerte!',
+        trigger: '4',
       },
       {
         id: '4',
+        message: '¿Cómo puedo ayudarte?',
+        trigger: '5',
+      },
+      {
+        id: '5',
+        options: [
+          { value: 1, label: 'Pre-venta', trigger: '4' },
+          { value: 2, label: 'Tarifas y recargos', trigger: '3' },
+          { value: 3, label: 'Política de privacidad', trigger: '3' },
+        ],
+      },
+      {
+        id: '6',
+        message: 'Wrong answer, try again.',
+        trigger: '5',
+      },
+      {
+        id: '7',
         message: 'Awesome! You are a telepath!',
         end: true,
       },
