68ca55b90713aef49469d6d5b1dd170aaf3eb0de
Yudith <yudith@MacBook-Air-de-administrador.local>
2020-09-03 22:02:55 -0500

    images

--- latintv/src/Components/ProgramModal.jsx
+++ latintv/src/Components/ProgramModal.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React , { useState }from 'react';
 import './styles/ProgramModal.scss';
+import { useEffect } from 'react';
+import { getAllData } from '../firebase/firestore'
 
 const ProgramModal = (props) => {
     const { data, show } = props;
+    const [img, setImg] = useState('');
+
+    useEffect(() => {
+        getAllData((programs) => {
+            const programTv = (programs.filter((program) => program.nombre === data.title));
+            setImg(programTv[0].img);
+         }, 'tvprograms');
+    }, []);
+    console.log(img);
     return (
         <React.Fragment>
                 <aside className="pmodal">
                         <div className="content-pmodal">
-                            <img src={require('../Pages/styles/assets/programa.png') }/>
+                            <img src={img} />
                             <button className="close-pmodal" onClick ={ () => show(false)}>X</button>
                             <p>Programa Aplicado: {data.title}</p>
                         </div>
