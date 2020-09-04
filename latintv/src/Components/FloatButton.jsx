7530722e07a65d554e69dad8ab9e16d507f9d9d7
IsabelaSanchez <natyst_26@hotmail.com>
2020-09-04 02:02:15 -0500

    adding chat bot and float button

--- latintv/src/Components/FloatButton.jsx
+++ latintv/src/Components/FloatButton.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import './styles/ChatBotLatin.scss';
+import './styles/FloatButton.scss';
+import boticon from './styles/assets/bot-icon.svg';
+import ChatBotLatin from './ChatBotLatin.jsx'
 
 const FloatButton = () => {
     return (
         <div class="btn-container">
             <input type="checkbox" id="btn-mas"></input>
+            <div className="redes">
+                <a href="#">
+                <ChatBotLatin/>
+                </a>
+            </div>
+            <div className="btn-mas1">
+                <label for="btn-mas" className="icon-mas2">
+                <img src={boticon} className="bot-icon" alt="boticon" />
+                </label>
+            </div>
         </div>
     )
 };
